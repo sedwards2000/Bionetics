848fb3b6ddc31aef3c26c5948a9e9fbd14dae4d0
sedwards2000 <sam_la_edwards@msn.com>
2020-09-22 16:36:27 +0100

    changed images on scrolling section

--- static/scripts.js
+++ static/scripts.js
@@ -21,11 +21,17 @@ function selectDivs(n){
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
+  var y = document.getElementsByClassName("badge")
   if (n > x.length){slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
   x[slideIndex-1].style.display = "block";
-  
+ 
+  for (i = 0; i < y.length; i++) {
+    y[i].classList.remove("yellow");  
+    y[i].classList.add("hoverYellow");  
+  }
+  y[slideIndex-1].classList.add("yellow");
 }
